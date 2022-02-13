3ba40ccc058051720e0837311611f4cbc170b2e4
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-13 20:27:57 +0000

    more OO

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+
+    logState: function(){
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
   return task;
-}
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(task) {
-task.complete = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
 }
 
 // DRIVER CODE BELOW
@@ -22,8 +21,8 @@ const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter b
 const task2 = newTask("Do Laundry", '😨');
 const tasks = [task1, task2];
 
-logTaskState(task1);
-completeTask(task1);
-logTaskState(task1);
+task1.logState();
+task1.markCompleted();
+task1.logState();
 
-console.log(tasks);
+
